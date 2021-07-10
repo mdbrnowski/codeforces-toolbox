1b79a3f51b1cb373a503429da7d34a0777d3701e
Michał Dobranowski <mdbrnowski@gmail.com>
2021-07-10 12:44:58 +0200

    Add basic structure to the 'test' command

--- cft/test.py
+++ cft/test.py
@@ -1,3 +1,6 @@
+import os
+
+
 def add_subcommand_test(subparsers):
     parser = subparsers.add_parser('test', help='test the solution file')
     parser.add_argument('task', type=str, help='task id in the form "A" or "9999A"')
@@ -5,5 +8,18 @@ def add_subcommand_test(subparsers):
 
 
 def test(args):
-    print('Testing task', args.task)
-    # TODO
+    task = args.task
+    if len(task) <= 2:
+        task_letter = task
+        task = os.path.basename(os.getcwd()) + task
+    else:
+        task_letter = task[4:]
+
+    if os.path.exists(task_letter):
+        os.chdir(task_letter)
+
+    if not os.path.exists('in'):
+        os.makedirs('in'), os.makedirs('out')
+        # TODO: download tests from statement
+
+    # TODO: execute tests and compare output
