9eb9ae63e7031e61ba5769a6a98c41953348865b
Michał Dobranowski <mdbrnowski@gmail.com>
2021-07-13 16:57:29 +0200

    Add 'test' functionality (download and run)

--- README.md
+++ README.md
@@ -4,6 +4,26 @@ This tool is created for the purpose of participation in contests on Codeforces 
 
 ## Usage
 
+`cft -h`
+
+```
+usage: cft [-h] {race,submit,test,config} ...
+
+Codeforces tool
+
+positional arguments:
+  {race,submit,test,config}
+    race                create folder and solution files based on the template
+    submit              submit solution
+    test                test the solution file
+    config              change configuration of the cft
+
+optional arguments:
+  -h, --help            show this help message and exit
+
+Wish you high ratings!
+```
+
 ### Template, login, password
 
 `cft config`
@@ -13,7 +33,7 @@ Choose one of the following (type an integer):
   1. change the template file
   2. change username and password
   3. change password
- ```
+```
 then you can just type an integer (1-3) and change your template file, username or password.
 
 ### Creating structure for a contest
@@ -39,6 +59,14 @@ then you can just type an integer (1-3) and change your template file, username 
 ```
 
 files 1234*X*.cpp are created by copying the previously specified template file.
+
+It is recommended that you then change the current working directory (`cd 1234`) to operate more smoothly later.
+
+### Testing solution
+
+`cft test 1234A` downloads (if not yet downloaded) example test and check if it passes. Your solution should be already compiled.
+
+If your current working directory is `some\path\1234` you can type just `cft test A`. 
 
 
 _**Other functionalities are in progress and will be released in a few days (hopefully).**_
--- cft/test.py
+++ cft/test.py
@@ -1,4 +1,10 @@
 import os
+import sys
+import requests
+import bs4
+import subprocess
+from colorama import init as colorama_init
+from termcolor import cprint
 
 
 def add_subcommand_test(subparsers):
@@ -8,18 +14,65 @@ def add_subcommand_test(subparsers):
 
 
 def test(args):
+    colorama_init()
     task = args.task
     if len(task) <= 2:
         task_letter = task
-        task = os.path.basename(os.getcwd()) + task
+        contest = os.path.basename(os.getcwd())
     else:
         task_letter = task[4:]
+        contest = task[:4]
 
     if os.path.exists(task_letter):
         os.chdir(task_letter)
 
     if not os.path.exists('in'):
-        os.makedirs('in'), os.makedirs('out')
-        # TODO: download tests from statement
+        os.makedirs('in')
+    if not os.path .exists('ans'):
+        os.makedirs('ans')
 
-    # TODO: execute tests and compare output
+    if len(os.listdir('in')) == 0:
+        r = requests.get(f'https://codeforces.com/problemset/problem/{contest}/{task_letter}', )
+        try:
+            r.raise_for_status()
+        except requests.HTTPError:
+            cprint("Sorry, something went wrong while downloading tests", 'red', 'on_white')
+            sys.exit()
+
+        soup = bs4.BeautifulSoup(r.text, 'html.parser')
+        tests_input = soup.select('div.sample-test div.input pre')
+        tests_answer = soup.select('div.sample-test div.output pre')
+        for i, (test_in, test_ans) in enumerate(zip(tests_input, tests_answer), start=1):
+            with open(f'in\\{i}.in', 'w') as input_file:
+                input_file.write(test_in.string.lstrip())
+            with open(f'ans\\{i}.out', 'w') as answer_file:
+                answer_file.write(test_ans.string.lstrip())
+
+    test_solution_file(contest + task_letter)
+
+
+def test_solution_file(task):
+    i = 1
+    while os.path.exists(f'in\\{i}.in'):
+        with open(f'in\\{i}.in') as input_file:
+            test_in = input_file.read()
+        with open(f'ans\\{i}.out') as answer_file:
+            test_ans = answer_file.read()
+        proc = subprocess.Popen([task], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        try:
+            test_out, test_err = proc.communicate(input=bytes(test_in, 'utf-8'), timeout=5)
+        except subprocess.TimeoutExpired:
+            proc.kill()
+            cprint('Execution time exceeded 5 seconds', 'red')
+            sys.exit()
+
+        test_out = test_out.decode('utf-8').lstrip()
+
+        if test_out.split() == test_ans.split():    # delete every whitespace
+            cprint('Test passed', 'green')
+        else:
+            cprint('Test did not pass', 'red')
+            print('Program output:', test_out, sep='\n')
+            print('Answer:', test_ans, sep='\n')
+
+        i += 1
